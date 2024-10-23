c6be0f18e519efca5b0b046f6d19d2a34257b968
Peter <62666278+GringoXY@users.noreply.github.com>
2024-10-23 21:43:43 +0200

    Simple calculator layout (#8)
    * add simple calculator layout
    
    * add switching to simple calculator activity
    
    * add styling
    
    * add simple calculator responsiveness
    
    * add simple calculator functionality
    
    * group buttons ids
    
    * add saving result state
    
    * improve sizes

--- app/src/main/java/pam/calculator/MainActivity.kt
+++ app/src/main/java/pam/calculator/MainActivity.kt
@@ -1,15 +1,38 @@
 package pam.calculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnSimple: Button;
+
+    private val menuMap: Map<Int, Class<*>> = mapOf(
+        R.layout.simple to SimpleActivity::class.java
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.menu)
+
+        findControls()
+        setOnClickListeners();
+    }
+
+    fun findControls() {
+        btnSimple = findViewById<Button>(R.id.btnSimple)
+    }
+
+    fun setOnClickListeners() {
+        btnSimple.setOnClickListener {
+            menuMap.get(R.layout.simple)?.let { c -> onMenuButtonClick(c) }
+        }
+    }
+
+    fun onMenuButtonClick(c: Class<*>) {
+        startActivity(Intent(this, c))
     }
 }
