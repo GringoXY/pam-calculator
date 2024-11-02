9fe7213a13ec7c2d0b01520efc26f9aa23c8a082
GringoXY <przemyslawkowalski01pk@gmail.com>
2024-11-02 16:56:15 +0100

    add 'About' menu button click handler

---
+++ app/src/main/java/pam/calculator/AboutActivity.kt
@@ -0,0 +1,15 @@
+package pam.calculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.about)
+    }
+
+}
